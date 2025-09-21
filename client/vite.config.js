51f4196a7179f9840727cdaede0d9fc800a1ce76
MANIOMARDEV <144290707+MANIOMARDEV@users.noreply.github.com>
2025-09-21 15:45:41 +0200

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   base: '/portfolio-mern/',
-  
+  preview: {
+    allowedHosts: ['myportfoliopro-18.onrender.com'],
+    port: 4173, // optional, can be changed if you want
+  },
 })
